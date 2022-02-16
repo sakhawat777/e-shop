50f584442d79b87db6ea8948d74e688ea5acddc3
sakhawat777 <biplobbms@gmail.com>
2022-02-16 13:20:17 +0600

    Banner Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header/Header';
+import Banner from './components/Banner/Banner';
 function App() {
 	return (
 		<div>
 			<Header />
+			<Banner />
 		</div>
 	);
 }
